38d0884dfa1b5ba37d294313e9829154eef841e2
nahid-priom <nahid.priom.06@gmail.com>
2023-08-22 02:27:59 +0600

    header and footer complete

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,15 +17,16 @@ const Hero = () => {
         </p>
         <CustomButton
           title="Explore Cars"
+          btnType="button"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-            <Image src='/hero.png' alt="hero" fill className="object-contain"/>
-            <div className="hero__image-overlay" />
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
